ed21b572a9b24fa9f2cebee0964ef4f0dd2e4e9b
Avi Halachmi (:avih) <avihpit@yahoo.com>
2021-09-22 03:40:59 +0300

    lua: fix static lib build, minor refinements
    The 5.2 branch seem to work well with -DBUILD_SHARED_LIBS=OFF, but
    the 5.1 branch doesn't. We'll handle it later.
    
    Also:
    - Disable RELATIVE_LOAD_LIB (cmake-build extension for non-windows)
    - Enable ANSI (mainline lua, avoids asm/other hacks).
    - pkg-config version: use src file (not git) - allows shallow clone.

--- pkg/lua.sh
+++ pkg/lua.sh
@@ -4,7 +4,13 @@ set -e
 BLD_DIR="$config_local_prefix"/tmp/${PKG}_build
 PREFIX="$config_local_prefix"/pkg/$PKG
 
-OPTIONS=
+# RELATIVE_LOADLIB is no-good cmake-build extension (expand ! in non-win paths).
+# USE_ANSI is mainline lua, only avoids hacks for "faster" cast to int.
+OPTIONS="
+    -DBUILD_SHARED_LIBS=OFF
+    -DLUA_USE_RELATIVE_LOADLIB=OFF
+    -DLUA_USE_ANSI=ON
+"
 
 if [ "$config_build_pic" = yes ]; then
     # untested, see https://stackoverflow.com/questions/38296756/what-is-the-idiomatic-way-in-cmake-to-add-the-fpic-compiler-option
@@ -27,7 +33,7 @@ pkg_configure() {
 
 pkg_build() {
     set -x
-    make -C "$BLD_DIR" install $OPTIONS "$@"
+    make -C "$BLD_DIR" install "$@"
 
     # and create a .pc file
     mkdir -p "$PREFIX"/lib/pkgconfig
@@ -38,7 +44,7 @@ includedir=\${prefix}/include
 
 Name: lua
 Description: Lua
-Version: $(cd "$config_lua_source" && git describe --tags)
+Version: $(grep version <"$PKG_source/dist.info" | sed 's/.*"\(.*\)".*/\1/')
 Libs: -L\${libdir} -llua
 Libs.private: -lm
 Cflags: -I\${includedir}
