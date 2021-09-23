3b4cb79d86508bcb17fc1006efd48857985f6090
Avi Halachmi (:avih) <avihpit@yahoo.com>
2021-09-23 14:25:12 +0300

    luajit: osx: set MACOSX_DEPLOYMENT_TARGET
    It's a system var which defaults to the current OS version if unset.
    
    LuaJIT requires that it's set, eventhough it doesn't actually use it.
    Now, on osx we set it to the current OS version if it's unset while
    building luajit.

--- pkg/luajit.sh
+++ pkg/luajit.sh
@@ -23,17 +23,35 @@ if [ "$config_build_host" ]; then
     fi
 fi
 
+# MACOSX_DEPLOYMENT_TARGET affects the system compiler and has system defaults
+# if it's unset, but luajit requires that it's set (but doesn't use it...).
+# Simply set it to the current OS version if unset (major.minor only - XX.YY)
+set_target() {
+    case $(uname) in *Darwin*)
+        if [ -z "${MACOSX_DEPLOYMENT_TARGET-}" ]; then
+            export MACOSX_DEPLOYMENT_TARGET="$(
+                v=$(sw_vers -productVersion || echo 10.12)  # arbitrary fallback
+                case $v in (*.*.*) v=${v%.*}; esac  # two numbers, not three
+                echo "$v"
+            )"
+            echo "+ export MACOSX_DEPLOYMENT_TARGET=$MACOSX_DEPLOYMENT_TARGET"
+        fi
+    esac
+}
+
 pkg_configure() {
     :
 }
 
 pkg_build() {
+    set_target
     set -x
     make -C "$PKG_source" install PREFIX="$PREFIX" $OPTIONS "$@"
     pkg_cp_pc "$PREFIX"
 }
 
 pkg_clean() {
+    set_target
     set -x
     make -C "$PKG_source" clean
 }
